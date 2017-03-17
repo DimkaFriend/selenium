4920cebae9a6589121a25294945f23bb858cf720
DimkaFriend <boltenkovdmitriyaleks@gmail.com>
2017-03-17 18:24:23 +0300

    Запустил на конец то gecodriver

--- untitled/javaHomeWork/src/test/java/test.java
+++ untitled/javaHomeWork/src/test/java/test.java
@@ -4,8 +4,16 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.firefox.internal.ProfilesIni;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import sun.java2d.cmm.Profile;
+
+import java.io.File;
 
 /**
  * Created by boltenkov on 15.03.2017.
@@ -17,20 +25,23 @@ public class test {
     @Before
     public void start()
     {
-        this.driver= new ChromeDriver();
+        System.setProperty("webdriver.gecko.driver", "C:\\Tools\\geckodriver.exe");
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability(FirefoxDriver.MARIONETTE,true);
+        caps.setCapability("unexpectedAlertBehaviour","ignore");
+        this.driver= new FirefoxDriver(caps);
     }
 
     @Test
     public void myFirst()
     {
-        this.driver.get("http://admin:admin@localhost:81/lifecart/admin/");// почему то всплавал alert для basic авторизации, эту  строчку наверное можно считать  костылем.
+        this.driver.get("http://localhost:81/lifecart/admin/");// почему то всплавал alert для basic авторизации, эту  строчку наверное можно считать  костылем.
         this.driver.findElement(By.name("username")).clear();
         this.driver.findElement(By.name("username")).sendKeys("admin");
         this.driver.findElement(By.name("password")).clear();
         this.driver.findElement(By.name("password")).sendKeys("admin");
         this.driver.findElement(By.name("login")).click();
-        new WebDriverWait(this.driver, 10).until(ExpectedConditions.titleIs("My Store"));
-        this.driver.findElement(By.linkText("Appearence")).click();
+
 
     }
 
