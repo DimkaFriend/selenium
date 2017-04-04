31f04c44b97425da9141ec000e81c6edd156cbba
DimkaFriend <boltenkovdmitriyaleks@gmail.com>
2017-04-04 08:01:27 +0300

    Модуль 4, ДЗ7 и ДЗ8 untitled\javaHomeWork\src\test\java\Modul_4\HomeWork7\TestGoogleChrome.java untitled\javaHomeWork\src\test\java\Modul_4\HomeWork7\TestGoogleChrome.java

---
+++ untitled/javaHomeWork/src/test/java/Modul_4/HomeWork7/TestGoogleChrome.java
@@ -0,0 +1,58 @@
+package Modul_4.HomeWork7;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+/**
+ * Created by boltenkov on 24.03.2017.
+ */
+public class TestGoogleChrome {
+    WebDriver driver;
+    @Before
+    public void start()
+    {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("unexpectedAlertBehaviour","ignore");
+        this.driver= new ChromeDriver(caps);
+    }
+
+    @Test
+    public void stepClickAllPointMenu()
+    {
+        this.driver.get("http://localhost:81/lifecart/admin/");
+        this.driver.findElement(By.name("username")).clear();
+        this.driver.findElement(By.name("username")).sendKeys("admin");
+        this.driver.findElement(By.name("password")).clear();
+        this.driver.findElement(By.name("password")).sendKeys("admin");
+        this.driver.findElement(By.name("login")).click();
+        int sizeLevelOne =this.driver.findElements(By.cssSelector("#app->a")).size();
+        for(int i = 0; i < sizeLevelOne; i ++)
+        {
+            this.driver.findElements(By.cssSelector("#app->a")).get(i).click();
+            if(this.driver.findElements(By.cssSelector("#app->ul>li>a>span.name")).size()!=0)
+            {
+                int  sizeLevelTwo =this.driver.findElements(By.cssSelector("#app->ul>li>a>span.name")).size();
+                for(int j = 0; j < sizeLevelTwo; j ++)
+                {
+                    this.driver.findElements(By.cssSelector("#app->ul>li>a>span.name")).get(j).click();
+                    if(this.driver.findElement(By.cssSelector("#content>h1")).isDisplayed()&&this.driver.findElement(By.cssSelector("#content>h1")).isEnabled())
+                    {
+                        System.out.println(this.driver.findElement(By.cssSelector("#content>h1")).getText().toString());
+                    }
+                }
+            }
+        }
+    }
+
+    @After
+    public void stop()
+    {
+        this.driver.quit();
+        this.driver = null;
+    }
+}
---
+++ untitled/javaHomeWork/src/test/java/Modul_4/HomeWork8/TestGoogleChrome.java
@@ -0,0 +1,52 @@
+package Modul_4.HomeWork8;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.List;
+
+/**
+ * Created by boltenkov on 24.03.2017.
+ */
+public class TestGoogleChrome {
+    WebDriver driver;
+    @Before
+    public void start()
+    {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("unexpectedAlertBehaviour","ignore");
+        this.driver= new ChromeDriver(caps);
+        this.driver.manage().window().maximize();
+    }
+
+    @Test
+    public void stickerOnProduct()
+    {
+        this.driver.get("http://localhost:81/lifecart");
+        List<WebElement> product =  this.driver.findElements(By.cssSelector("li>a.link"));
+        this.driver.findElements(By.cssSelector("li>a.link"))
+                .forEach(WebElement->
+                {
+                   if(WebElement.findElements(By.cssSelector("li>a.link>div>div.sticker")).size()!=1)
+                   {
+                       new ArrayIndexOutOfBoundsException().printStackTrace();
+                       System.out.println( WebElement.getLocation());
+                   }
+                });
+
+    }
+
+    @After
+    public void stop()
+    {
+        this.driver.quit();
+        this.driver = null;
+    }
+}
